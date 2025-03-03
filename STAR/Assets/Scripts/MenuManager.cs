8db0a391252717e042a5a7482b457444ae2f056f
RHUBDev <roberthendrygames@gmail.com>
2025-03-03 16:26:45 +0000

    Added Music and SFX volume sliders and Back Button to settings menu in MainMenu scene

--- STAR/Assets/Scripts/MenuManager.cs
+++ STAR/Assets/Scripts/MenuManager.cs
@@ -8,7 +8,9 @@ public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance;
 
-    public Slider volumeSlider;
+    public Slider volumeSlider; 
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
 
     [SerializeField] private AudioMixer audioMixer;
 
@@ -26,8 +28,16 @@ public class MenuManager : MonoBehaviour
         if (!PlayerPrefs.HasKey("masterVolume"))
         {
             PlayerPrefs.SetFloat("masterVolume", 0.8f);
-            LoadVolume();
         }
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 0.8f);
+        }
+        if (!PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("sfxVolume", 0.8f);
+        }
+        LoadVolume();
     }
 
     // Update is called once per frame
@@ -41,17 +51,24 @@ public class MenuManager : MonoBehaviour
     }
     public void ChangeVolume()
     {
-        AudioListener.volume = volumeSlider.value;
         SaveVolume();
     }
     public void LoadVolume()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
         audioMixer.SetFloat("MasterVol", ((100 * volumeSlider.value) - 80f));
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        audioMixer.SetFloat("MusicVol", ((100 * musicVolumeSlider.value) - 80f));
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        audioMixer.SetFloat("SFXVol", ((100 * sfxVolumeSlider.value) - 80f));
     }
     public void SaveVolume()
     {
         PlayerPrefs.SetFloat("masterVolume", volumeSlider.value);
-        audioMixer.SetFloat("MasterVol", ((100 * volumeSlider.value) -80f));
+        audioMixer.SetFloat("MasterVol", ((100 * volumeSlider.value) - 80f));
+        PlayerPrefs.SetFloat("musicVolume", musicVolumeSlider.value);
+        audioMixer.SetFloat("MusicVol", ((100 * musicVolumeSlider.value) - 80f));
+        PlayerPrefs.SetFloat("sfxVolume", sfxVolumeSlider.value);
+        audioMixer.SetFloat("SFXVol", ((100 * sfxVolumeSlider.value) - 80f));
     }
 }
