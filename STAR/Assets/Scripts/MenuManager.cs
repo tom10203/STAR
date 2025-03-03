a8306f635192228ec5586a34381a5b7e12d30fa6
AlexanderTheAlright98 <Alexanderthealright@outlook.com>
2025-03-03 15:04:58 +0000

    Made basic volume slider in MainManager
    This will be changed/expanded on shortly by (possibly) Robert

--- STAR/Assets/Scripts/MenuManager.cs
+++ STAR/Assets/Scripts/MenuManager.cs
@@ -1,12 +1,30 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static MenuManager Instance;
+
+    public Slider volumeSlider;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("masterVolume"))
+        {
+            PlayerPrefs.SetFloat("masterVolume", 0.8f);
+            LoadVolume();
+        }
     }
 
     // Update is called once per frame
@@ -17,5 +35,18 @@ public class MenuManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolume();
+    }
+    public void LoadVolume()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+    }
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("masterVolume", volumeSlider.value);
     }
 }
