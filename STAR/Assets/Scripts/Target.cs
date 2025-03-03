c9ccdddb401d42add25ce7540d8641dca3897173
RHUBDev <roberthendrygames@gmail.com>
2025-03-03 13:05:06 +0000

    Added Targets

---
+++ STAR/Assets/Scripts/Target.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] Animator targetAnim;
+    private bool gotHit = false;
+    [SerializeField] float timeToAdd = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TargetHit()
+    {
+        if (!gotHit)
+        {
+            gotHit = true;
+            //ToDo - add remove time/points
+
+            audio.PlayOneShot(hitSound, 1f);
+            targetAnim.SetTrigger("TargetHit");
+        }
+    }
+}
