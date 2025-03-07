972cc7f2682f61f21d65e0c95aba6408ee628e80
AlexanderTheAlright98 <Alexanderthealright@outlook.com>
2025-03-07 10:37:03 +0000

    Worked on level selection and the win/lose screen at the end of levels

--- STAR/Assets/Scripts/MenuManager.cs
+++ STAR/Assets/Scripts/MenuManager.cs
@@ -52,11 +52,6 @@ public class MenuManager : MonoBehaviour
         LoadMouse();
         #endregion
     }
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
-    }
-
     #region Volume Functions
     public void ChangeVolume()
     {
@@ -93,6 +88,25 @@ public class MenuManager : MonoBehaviour
     public void SaveMouse()
     {
         PlayerPrefs.SetFloat("mouseSensitivity", mouseSlider.value);
+    }
+    #endregion
+    #region Level Buttons
+    //This probably isn't the ideal way of doing this, but it's quick and easy!
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
     }
     #endregion
 }
