c10b8d7bcdfa25def9a242ba5b5d99d91aa47ec8
RHUBDev <roberthendrygames@gmail.com>
2025-03-07 10:44:46 +0000

    Added InGameUI script

---
+++ STAR/Assets/Scripts/InGameUI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+
+
+public class InGameUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    //float elapsedTime;
+    [SerializeField] float remainingTime;
+
+    public bool hasTimerStarted = false;
+
+    private void Update()
+    {
+        if (remainingTime > 0) 
+        {
+          
+            if (hasTimerStarted)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            remainingTime = 0;
+            //Gameover();
+
+            timerText.color = Color.red;
+
+        }
+
+
+
+
+        //Timer counting up from 00:00
+        //elapsedTime += Time.deltaTime;
+        //int minuets = Mathf.FloorToInt(elapsedTime / 60);
+        //int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        //timerText.text = string.Format("{0:00} : {1:00}", minuets, seconds);
+
+        //Timer countdown
+        
+        int minuets = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        
+        timerText.text = string.Format("{0:00} : {1:00}", minuets, seconds);
+
+
+
+    }
+
+
+
+}
