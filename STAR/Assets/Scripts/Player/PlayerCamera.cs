c6e07c5653fe31eceb56706240c827f30c1d951b
RHUBDev <roberthendrygames@gmail.com>
2025-03-11 16:59:19 +0000

    Camera is now limited to +/- 85 degrees

--- STAR/Assets/Scripts/Player/PlayerCamera.cs
+++ STAR/Assets/Scripts/Player/PlayerCamera.cs
@@ -8,11 +8,12 @@ public struct CameraInput
 public class PlayerCamera : MonoBehaviour
 {
     private Vector3 _eulerAngles;
+    private float camLimitX = 85;
 
     [SerializeField] private float sensitivity = 0.1f;
     public void Initialise(Transform target)
     {
-        sensitivity = PlayerPrefs.GetFloat("mouseSensitivity");
+        sensitivity = PlayerPrefs.GetFloat("mouseSensitivity", 0.1f);
         transform.position = target.position;
         transform.eulerAngles = _eulerAngles = target.eulerAngles;
     }
@@ -25,7 +26,10 @@ public class PlayerCamera : MonoBehaviour
     public void UpdateRotation(CameraInput input)
     {
         _eulerAngles += new Vector3(-input.look.y, input.look.x) * sensitivity;
-        var eulerAnglesX = _eulerAngles.x;
+        /*var eulerAnglesX = _eulerAngles.x;
+        transform.eulerAngles = _eulerAngles;*/
+        var eulerAnglesX = Mathf.Clamp(_eulerAngles.x, -camLimitX, camLimitX);
+        _eulerAngles = new Vector3(eulerAnglesX, _eulerAngles.y, _eulerAngles.z);
         transform.eulerAngles = _eulerAngles;
     }
 }
